01177dff2808526d927a4552524acc70bf7827fe
Aurelia F. Simatupang <dorm4ever@gmail.com>
2024-02-16 13:00:33 +0100

    basic html structure

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function showAnswer(response) {
-    alert(response.data.answer);
+let apiKey = '2f06037930c0t042fb8364af86adf19o';
+let apiUrl = '';
+let context = '';
+let promptText = '';
+
+const getAI = (event) => {
+    event.preventDefault();
+    console.log(event.target[0]);
+    // console.log('Processing');
+    // context = 'be polite and provide a very short answer';
+    // promptText = 'Who the first sfemale president was';
+    // apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    // axios.get(apiUrl).then(showPoem);
 }
 
-let apiKey = '2f06037930c0t042fb8364af86adf19o';
-let context = 'be polite and provide a very short answer';
-let prompt = 'Who the first sfemale president was';
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+const formComponenet = document.querySelector("form");
+formComponenet.addEventListener("submit", getAI);
 
-console.log('Processing');
-axios.get(apiUrl).then(showAnswer);
+const showPoem = () => {
+    new Typewriter("#poem", {
+        strings: [response.data.answer],
+        autoStart: true,
+      });
+}
